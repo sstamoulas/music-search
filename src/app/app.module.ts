3cf66a295837716cc5fb1d474d726831bfe3d45e
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2021-10-27 21:45:09 +0300

    Added Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { 
+  LocationStrategy, 
+  HashLocationStrategy, 
+  APP_BASE_HREF 
+} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AlbumComponent } from './album/album.component';
+// import { ArtistComponent } from './artist/artist.component';
 
 @NgModule({
   declarations: [
